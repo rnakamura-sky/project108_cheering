46f5fadac0fea21ab3e4c7ace5e6b8986a0ad437
rnakamura <rnakamura@rnakamuranoMacBook-Pro.local>
2020-09-18 23:46:44 +0900

    カスタマイズユーザーを作成

--- api/project108/project108/settings.py
+++ api/project108/project108/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',   # 追加
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+AUTH_USER_MODEL = 'account.User'
